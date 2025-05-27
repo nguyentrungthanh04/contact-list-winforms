4da91a26af006efa03d20a82322b0b83236aecbe
sinnt <sinnt@VYNEE>
2025-05-27 09:20:23 +0700

    Xoa dong code thua sau khi merge

--- ContactList/Form1.Designer.cs
+++ ContactList/Form1.Designer.cs
@@ -51,7 +51,6 @@
             this.button1.TabIndex = 1;
             this.button1.Text = "Thêm liên hệ";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
